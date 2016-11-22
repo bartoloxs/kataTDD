c8baf9f01899cab813755eadc6337a2e453a9d57
Carlos Barrón <carlosjesus.barron@bbva.com>
2016-11-22 15:45:29 +0100

    [KATA-TDD] kata 1

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -1,7 +1,36 @@
-var assert = require('assert');
+'use-strict';
 
-describe('Be welcome to Mocha', function() {
-  it('with a failing test', function() {
-    assert(false, 'Hello World!');
+var assert = require('assert'),
+    chai = require('chai'),
+    expect = chai.expect,
+    validator = require('../lib/validator');
+
+describe('A Validator', function() {
+  it('will return no error for valid numbers', function() {
+    expect(validator(7)).to.be.empty;
+  });
+  describe('will return error.nonpositive for not strictly positive numbers', function() {
+    it('like 0', function() {
+      expect(validator(0)).to.include('error.nonpositive');
+    });
+    it('like -2', function() {
+      expect(validator(-2)).to.include('error.nonpositive');
+    });
+  });
+  describe('will return error.three for divisible by 3 numbers:', function() {
+    it('like 3', function() {
+      expect(validator(3)).to.include('error.three');
+    });
+    it('like 15', function() {
+      expect(validator(15)).to.be.include('error.five');
+    });
+  });
+  describe('will return error.five for divisible by 5 numbers:', function() {
+    it('like 5', function() {
+      expect(validator(5)).to.include('error.five');
+    });
+    it('like 15', function() {
+      expect(validator(15)).to.include('error.five');
+    });
   });
 });
